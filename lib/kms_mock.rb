8e81329cda9672f30a224f3c9c903ca6a980e8f4
Ash Tyndall <git@atyndall.net>
2016-11-25 09:20:08 +0800

    Add NotImplementedError when trying to use encryption context with the mock

--- lib/kms_mock.rb
+++ lib/kms_mock.rb
@@ -7,6 +7,7 @@ module Aws
 
       def generate_data_key(key_id:, key_spec:, encryption_context: nil)
         raise RuntimeError, 'Unsupported key_spec in test mode' unless key_spec == 'AES_256'
+        raise NotImplementedError, 'Encryption context not current supported in test mode' unless encryption_context.nil?
 
         plaintext = SecureRandom.random_bytes(256/8)
         ciphertext_blob = plaintext.reverse
@@ -21,6 +22,7 @@ module Aws
 
       def decrypt(ciphertext_blob:, encryption_context: nil)
         raise Aws::KMS::Errors::InvalidCiphertextException.new(nil, nil) unless @@blob_to_key.include?(ciphertext_blob)
+        raise NotImplementedError, 'Encryption context not current supported in test mode' unless encryption_context.nil?
 
         Aws::KMS::Types::DecryptResponse.new(
           key_id: @@blob_to_key[ciphertext_blob],
