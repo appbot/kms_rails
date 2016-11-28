00fbbc089ec843efbf84685a6e83962026ea16df
Ash Tyndall <git@atyndall.net>
2016-11-28 14:16:58 +0800

    Rework mock so it sorts its metadata within the cyphertext blob

--- lib/kms_rails/kms_client_mock.rb
+++ lib/kms_rails/kms_client_mock.rb
@@ -1,34 +1,32 @@
 require 'aws-sdk'
+require 'msgpack'
 
 module KmsRails
   module Aws
     module KMS
       class Client
-        @@blob_to_key = {}
-
         def generate_data_key(key_id:, key_spec:, encryption_context: nil)
           raise RuntimeError, 'Unsupported key_spec in test mode' unless key_spec == 'AES_256'
-          raise NotImplementedError, 'Encryption context not current supported in test mode' unless encryption_context.nil?
 
           plaintext = SecureRandom.random_bytes(256/8)
-          ciphertext_blob = plaintext.reverse
-          @@blob_to_key[ciphertext_blob] = key_id
 
           ::Aws::KMS::Types::GenerateDataKeyResponse.new(
             key_id: key_id,
             plaintext: plaintext,
-            ciphertext_blob: ciphertext_blob,
+            ciphertext_blob: [key_id, encryption_context, plaintext].to_msgpack.reverse,
           )
         end
 
         def decrypt(ciphertext_blob:, encryption_context: nil)
-          raise ::Aws::KMS::Errors::InvalidCiphertextException.new(nil, nil) unless @@blob_to_key.include?(ciphertext_blob)
-          raise NotImplementedError, 'Encryption context not current supported in test mode' unless encryption_context.nil?
+          key_id, decoded_context, plaintext = MessagePack.unpack(ciphertext_blob.reverse)
+          raise ::Aws::KMS::Errors::InvalidCiphertextException.new(nil, nil) unless decoded_context == encryption_context
 
           ::Aws::KMS::Types::DecryptResponse.new(
-            key_id: @@blob_to_key[ciphertext_blob],
-            plaintext: ciphertext_blob.reverse,
+            key_id: key_id,
+            plaintext: plaintext,
           )
+        rescue MessagePack::MalformedFormatError
+          raise ::Aws::KMS::Errors::InvalidCiphertextException.new(nil, nil)
         end
 
         def inspect
